43d3a89271ba91cda1705cf973c6774fb1dac809
Farah Seltany <farahseltany@gmail.com>
2021-01-12 14:04:21 +0100

    add vue router

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -1,48 +1,25 @@
 <template>
   <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <router-link
-        class="navbar-brand"
-        to="/home"
-      >
-        App
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div
-        id="navbarNav"
-        class="collapse navbar-collapse"
-      >
-        <ul class="navbar-nav">
-          <router-link
-            class="nav-item"
-            tag="li"
-            to="/home"
-            active-class="active"
-          >
-            <a class="nav-link">Home</a>
-          </router-link>
-        </ul>
-      </div>
-    </nav>
-
-    <router-view />
+    <Navbar />
+   
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar"
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Navbar: Navbar
+  },
 };
 </script>
 <style lang="scss" scoped>
+.container-fluid {
+    padding: 0;
+}
 </style>
