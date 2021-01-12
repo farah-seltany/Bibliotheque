0a9bc7dbefe9ae53154bc3d131fd932f7220ba40
Farah Seltany <farahseltany@gmail.com>
2021-01-12 16:21:30 +0100

    add navbar component and database fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,25 +7,11 @@ use App\Entity\Book;
 use App\Entity\Category;
 use App\Entity\Editor;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    /**
-     * @var CategoriesRepository
-     */
-
-    public $repository;
-
-        public function __construct(EntityManagerInterface $em)
-        {
-            $this->repository = $em->getRepository(Categories::class);
-            dd($this->repository);
-
-        }
-
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
@@ -33,7 +19,27 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('fr_FR');
 
-        
+            for ($a = 0; $a < 5; $a++) {
+                $author = new Author();
+                $author->setFirstname($faker->firstName)
+                    ->setLastname($faker->lastName)
+                    ->setBirthday($faker->dateTime($max = 'now', $timezone = null));
+    
+                $manager->persist($author);
 
+                for ($b = $a; $b < $a + 5; $b++) {
+                    $book = new Book();
+                    $book->setAuthor($author)
+                        ->setTitle($faker->word)
+                        ->setSlug("Slug-$b")
+                        ->setIsbn($faker->isbn10)
+                        ->setResume("Resume $b")
+                        ->setPrice($faker->randomFloat($nbDecimals = 2, $min = 10, $max = 1000));
+
+                    $manager->persist($book);
+                }
+            }
+
+        $manager->flush();
     }
 }
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -10,7 +10,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=AuthorRepository::class)
- * @ApiResource
+ *     * @ApiResource
  */
 class Author
 {
